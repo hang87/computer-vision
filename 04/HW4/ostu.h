58f21e05cab8aecd5676320032a14c7e8e159aae
jzin <670163670@qq.com>
2019-10-27 13:23:03 +0800

    拒绝重复造轮子

--- 04/HW4/ostu.h
+++ 04/HW4/ostu.h
@@ -7,7 +7,6 @@
 using namespace cimg_library;
 using namespace std;
 
-// https://linjiafengyang.github.io/2018/05/20/Ex5%EF%BC%9A%E5%9B%BE%E5%83%8F%E5%88%86%E5%89%B2/
 class OSTU {
 public:
     OSTU(string);
